dda4e50e174026290fb6f5f47320cab087cbb3d0
jeff john <jeffsams@gmail.com>
2017-07-20 18:52:49 -0400

    Game Playable

--- Assets/__Scripts/CardBartok.cs
+++ Assets/__Scripts/CardBartok.cs
@@ -37,7 +37,7 @@ public class CardBartok : Card
 
     // When the card is done moving, it will call reportFinishTo.SendMessage()
     public GameObject reportFinishTo = null;
-
+    public Player callbackPlayer = null;
     // MoveTo tells the card to interpolate to a new position and rotation
     public void MoveTo(Vector3 ePos, Quaternion eRot)
     {
@@ -115,6 +115,13 @@ public class CardBartok : Card
                         //  to the same GameObject every subsequent time it moves.
                         reportFinishTo = null;
                     }
+                    else if (callbackPlayer != null)
+                    {
+                        // If there's a callback Player
+                        // then call CBCallback directly on the Player
+                        callbackPlayer.CBCallback(this);
+                        callbackPlayer = null;
+                    }
                     else
                     { // If there is nothing to callback
                       // Do nothing
@@ -143,5 +150,18 @@ public class CardBartok : Card
                 break;
 
         }
+    }
+            // This allows the card to react to being clicked
+    override public void OnMouseUpAsButton()
+    {
+        // Call the CardClicked method on the Bartok singleton
+        Bartok.S.CardClicked(this);
+        // Also call the base class (Card.cs) version of this method
+        base.OnMouseUpAsButton();
+    }
+
+    void Awake()
+    {
+        callbackPlayer = null; // Just to be sure.
     }
 }
